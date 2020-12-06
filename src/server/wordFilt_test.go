5c32bd30a0ed2a215d379d40044054b0284449fe
youjiawei <fftyjw@qq.com>
2020-12-06 10:05:38 +0800

    add test string

--- src/server/wordFilt_test.go
+++ src/server/wordFilt_test.go
@@ -9,8 +9,10 @@ func TestWordFilt(t *testing.T) {
 	filter := NewTrie()
 	filter.Insert("abc")
 	fmt.Println(filter.Filt("abc"))
-	fmt.Println(filter.Filt("ab"))
+	fmt.Println(filter.Filt("a"))
 	fmt.Println(filter.Filt("b"))
 	fmt.Println(filter.Filt("c"))
+	fmt.Println(filter.Filt("ab"))
+	fmt.Println(filter.Filt("bc"))
 	fmt.Println(filter.Filt("abcd"))
 }
